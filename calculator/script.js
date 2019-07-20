d726172b2a648d24eeaae6718cb60c4732254cd9
Jean Jacques aka 1980 <jeremy.bertin76@hotmail.fr>
2019-07-20 09:32:14 +0200

    positive to negative value and vice versa with the plus, minus key

--- calculator/script.js
+++ calculator/script.js
@@ -35,10 +35,15 @@ let button2 = document.getElementsByClassName('button2');
 
 // show the numbers typed or result
 
-let preview = [""];
+let result = document.getElementById('result');
+
+let preview = [];
+let plusMinusResult = [];
 
 
 let buttonAc = document.getElementById('ac');
+let plusMinus = document.getElementById('plus-minus');
+let plusMinus2 = document.getElementById('plus-minus2');
 
 
 let buttonN0 = document.getElementById('zero');
@@ -53,10 +58,12 @@ let buttonN8 = document.getElementById('eight');
 let buttonN9 = document.getElementById('nine');
 
 // button AC erasing result and changing outlook to C when other buttons are clicked
+// number 0 disapear when there is only zero and when a key is clicked
 
 buttonAc.onclick = function () {
   buttonAc.innerHTML = "AC";
-  preview = [""];
+  preview = [];
+  plusMinusResult = [];
   document.getElementById('result').innerHTML = 0;
 }
 
@@ -66,68 +73,100 @@ buttonN0.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN0 = 0;
   preview.push(buttonN0);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN1.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN1 = 1;
   preview.push(buttonN1);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN2.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN2 = 2;
   preview.push(buttonN2);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN3.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN3 = 3;
   preview.push(buttonN3);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN4.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN4 = 4;
   preview.push(buttonN4);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN5.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN5 = 5;
   preview.push(buttonN5);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN6.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN6 = 6;
   preview.push(buttonN6);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN7.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN7 = 7;
   preview.push(buttonN7);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN8.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN8 = 8;
   preview.push(buttonN8);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
 }
 
 buttonN9.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN9 = 9;
   preview.push(buttonN9);
-  document.getElementById('result').innerHTML = preview.join('');
+  const a = preview.join('')
+  document.getElementById('result').innerHTML = a;
+}
+
+// positive to negative value and vice versa with the plus, minus key
+
+plusMinus.onclick = function(){
+
+  let a = preview.join('');
+  let b = parseInt(a, 10);
+  let c = b * -1;
+  // let d = c * -1;
+
+  result.innerHTML = c;
+  document.getElementById('plus-minus').style.display = "none";
+  document.getElementById('plus-minus2').style.display = "block";
+
+
+  plusMinus2.onclick = function(){
+
+    result.innerHTML = b;
+    document.getElementById('plus-minus2').style.display = "none";
+    document.getElementById('plus-minus').style.display = "block";
+  }
 }
