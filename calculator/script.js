796c707ef6b865e81ec956bd01131317e931ba0e
Jean Jacques aka 1980 <jeremy.bertin76@hotmail.fr>
2019-07-20 15:55:44 +0200

    added behavior that when the plus-minus key is hited then if we hit the key 9 (this is for the test) the screen from the calculator is starting from 0 again

--- calculator/script.js
+++ calculator/script.js
@@ -38,13 +38,12 @@ let button2 = document.getElementsByClassName('button2');
 let result = document.getElementById('result');
 
 let preview = [];
-let plusMinusResult = [];
+let preview2 = [];
 
 
 let buttonAc = document.getElementById('ac');
 let plusMinus = document.getElementById('plus-minus');
 let plusMinus2 = document.getElementById('plus-minus2');
-
 
 let buttonN0 = document.getElementById('zero');
 let buttonN1 = document.getElementById('one');
@@ -56,6 +55,20 @@ let buttonN6 = document.getElementById('six');
 let buttonN7 = document.getElementById('seven');
 let buttonN8 = document.getElementById('eight');
 let buttonN9 = document.getElementById('nine');
+// let allButtonsN = [buttonN0, buttonN1, buttonN2, buttonN3, buttonN4, buttonN5, buttonN6, buttonN7, buttonN8, buttonN9];
+
+let buttonN02 = document.getElementById('zero2');
+let buttonN12 = document.getElementById('one2');
+let buttonN22 = document.getElementById('two2');
+let buttonN32 = document.getElementById('three2');
+let buttonN42 = document.getElementById('four2');
+let buttonN52 = document.getElementById('five2');
+let buttonN62 = document.getElementById('six2');
+let buttonN72 = document.getElementById('seven2');
+let buttonN82 = document.getElementById('eight2');
+let buttonN92 = document.getElementById('nine2');
+// let allButtonsN2 = [buttonN02, buttonN12, buttonN22, buttonN32, buttonN42, buttonN52, buttonN62, buttonN72, buttonN82, buttonN92];
+
 
 // button AC erasing result and changing outlook to C when other buttons are clicked
 // number 0 disapear when there is only zero and when a key is clicked
@@ -63,8 +76,7 @@ let buttonN9 = document.getElementById('nine');
 buttonAc.onclick = function () {
   buttonAc.innerHTML = "AC";
   preview = [];
-  plusMinusResult = [];
-  document.getElementById('result').innerHTML = 0;
+  result.innerHTML = 0;
 }
 
 // concatenation of the buttons numbers without any commas
@@ -73,80 +85,80 @@ buttonN0.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN0 = 0;
   preview.push(buttonN0);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN1.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN1 = 1;
   preview.push(buttonN1);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN2.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN2 = 2;
   preview.push(buttonN2);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN3.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN3 = 3;
   preview.push(buttonN3);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN4.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN4 = 4;
   preview.push(buttonN4);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN5.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN5 = 5;
   preview.push(buttonN5);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN6.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN6 = 6;
   preview.push(buttonN6);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN7.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN7 = 7;
   preview.push(buttonN7);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN8.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN8 = 8;
   preview.push(buttonN8);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 buttonN9.onclick = function () {
   document.getElementById('ac').innerHTML = "C";
   buttonN9 = 9;
   preview.push(buttonN9);
-  const a = preview.join('')
-  document.getElementById('result').innerHTML = a;
+  const a = preview.join('');
+  result.innerHTML = a;
 }
 
 // positive to negative value and vice versa with the plus, minus key
@@ -156,17 +168,29 @@ plusMinus.onclick = function(){
   let a = preview.join('');
   let b = parseInt(a, 10);
   let c = b * -1;
-  // let d = c * -1;
 
   result.innerHTML = c;
-  document.getElementById('plus-minus').style.display = "none";
-  document.getElementById('plus-minus2').style.display = "block";
+  plusMinus.style.display = "none";
+  plusMinus2.style.display = "block";
+  
+  buttonN9.style.display = "none";
+  buttonN92.style.display = "block";
+}
 
+plusMinus2.onclick = function(){
 
-  plusMinus2.onclick = function(){
+  let a = preview.join('');
+  let b = parseInt(a, 10);
+  let c = b * -1;
 
-    result.innerHTML = b;
-    document.getElementById('plus-minus2').style.display = "none";
-    document.getElementById('plus-minus').style.display = "block";
-  }
+  result.innerHTML = b;
+  plusMinus2.style.display = "none";
+  plusMinus.style.display = "block";
+}
+
+buttonN92.onclick = function(){
+  result.innerHTML = 0;
+  preview = [];
+  buttonN92.style.display = "none";
+  buttonN9.style.display = "block";
 }
