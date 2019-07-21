6eab9dcf999d216a4074e9aefe519134d40b212d
Tino <valentinbuis@gmail.com>
2019-07-21 10:48:20 +0200

    improvement: created a function for the color change on mouseUp and mouseDown

--- calculator/script.js
+++ calculator/script.js
@@ -1,36 +1,24 @@
 // change the color of the button when mousedown/up
 
 let button = document.getElementsByClassName('button');
-  for(i = 0; i < button.length; i++){
-    button[i].onmousedown = function() {
-      this.style.backgroundColor = "#a3a3a3";
-    };
-
-    button[i].onmouseup = function() {
-      this.style.backgroundColor = "#808080";
-    };
-}
+changeBackgroundOnMouseEvents(button, "#a3a3a3", "#808080");
 
 let buttonLg = document.getElementsByClassName('button-lg');
-  for(i = 0; i < buttonLg.length; i++){
-    buttonLg[i].onmousedown = function() {
-      this.style.backgroundColor = "#a3a3a3";
-    };
-
-    buttonLg[i].onmouseup = function() {
-      this.style.backgroundColor = "#808080";
-    };
-}
+changeBackgroundOnMouseEvents(buttonLg, "#a3a3a3", "#808080");
 
 let button2 = document.getElementsByClassName('button2');
-  for(i = 0; i < button2.length; i++){
-    button2[i].onmousedown = function() {
-      this.style.backgroundColor = "#ffe299";
+changeBackgroundOnMouseEvents(button2, "#ffe299", "#ffca47");
+
+function changeBackgroundOnMouseEvents(htmlElements, mouseDownColor, mouseUpColor) {
+  for (i = 0; i < htmlElements.length; i++) {
+    htmlElements[i].onmousedown = function() {
+      this.style.backgroundColor = mouseDownColor;
     };
 
-    button2[i].onmouseup = function() {
-      this.style.backgroundColor = "#ffca47";
+    htmlElements[i].onmouseup = function() {
+      this.style.backgroundColor = mouseUpColor;
     };
+  }
 }
 
 // show the numbers typed or result
