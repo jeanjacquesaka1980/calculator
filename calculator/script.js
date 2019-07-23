2d94c02b6653a1724d0118ce1190b38a229987e0
Jean Jacques aka 1980 <jeremy.bertin76@hotmail.fr>
2019-07-23 17:04:55 +0200

    trying to add a behavior that reseting the screen when we click on the number buttons while % and plus/minus have been clicked previously. It's not working, i let instructions on the function percentageOfNumber

--- calculator/script.js
+++ calculator/script.js
@@ -115,6 +115,25 @@ buttonNumberHandler(buttonN9, 9);
 
 // positive to negative value and vice versa with the plus, minus key
 
+// function that reset the result to 0 and cancel the behavior of the plus minus handler
+function buttonNumberResetPlusMinus(bt1, bt2, el1, el2){
+  for(i = 0; i < bt1.length; i++){
+    bt1[i].onclick = function(){
+      preview = [];
+      el1 = preview;
+      el2 = el1;
+      result.innerHTML = 0;
+      plusMinus2.style.display = "none";
+      plusMinus.style.display = "block";
+
+      for(i = 0; i < allButtonsN.length; i++){
+        bt1[i].style.display = "none";
+        bt2[i].style.display = "block";
+      }
+    }
+  }
+}
+
 function buttonPlusMinusHandler(button1, button2, button3, button4){
   button1.onclick = function(){
 
@@ -140,38 +159,59 @@ function buttonPlusMinusHandler(button1, button2, button3, button4){
         button1.style.display = "block";
         }
       }
-
-      function buttonNumberResetPlusMinus(key1, key2){
-        for(i = 0; i < key1.length; i++){
-          key1[i].onclick = function(){
-            preview = [];
-            b = preview;
-            c = b;
-            result.innerHTML = 0;
-            button2.style.display = "none";
-            button1.style.display = "block";
-
-            for(i = 0; i < key2.length; i++){
-              key1[i].style.display = "none";
-              key2[i].style.display = "block";
-            }
-          }
-        }
-      }
-      buttonNumberResetPlusMinus(allButtonsN2, allButtonsN);
+      buttonNumberResetPlusMinus(allButtonsN2, allButtonsN, b, c);
     }
   }
 
 buttonPlusMinusHandler(plusMinus, plusMinus2, allButtonsN, allButtonsN2);
 
-function percentageOfNumber(button){
-  button.onclick = function(){
-    const a = preview.join('');
-    const b = a / 100;
-    result.innerHTML = b;
+function percentageOfNumber(button1, button2, button3){
+  button1.onclick = function(){
+    let a = preview.join('');
+    let b = parseInt(a, 10);
+    let c = b / 100;
+
+    result.innerHTML = c;
     preview = [];
+
+    button2.onclick = function(){
+
+      let d = c * -1;
+      result.innerHTML = d;
+      button2.style.display = "none";
+      button3.style.display = "block";
+      preview = [];
+
+      for(i = 0; i < allButtonsN.length || i < allButtonsN2.length; i++){
+        allButtonsN[i].style.display = "none";
+        allButtonsN2[i].style.display = "block";
+
+        allButtonsN2[i].onmousedown = function(){
+          result.innerHTML = 0;
+          // it's here that the problem is occuring. If you put in comments that two lines below and just
+          // drop a console.log("hello !") it will be no problem. From the step before, those same lines
+          // are beahving perfectly but the message is talking about the display ... 
+          allButtonsN2[i].style.display = "none";
+          allButtonsN[i].style.display = "block";
+
+          plusMinus2.style.display = "none";
+          plusMinus.style.display = "block";
+          preview = [];
+          a = preview;
+          b = a;
+          c = b;
+          d = c;
+        }
+      }
+
+      button3.onclick = function(){
+        result.innerHTML = d * -1;
+        button3.style.display = "none";
+        button2.style.display = "block";
+        preview = [];
+      }
+    }
   }
-  
 }
 
-percentageOfNumber(percentage);
+percentageOfNumber(percentage, plusMinus, plusMinus2);
